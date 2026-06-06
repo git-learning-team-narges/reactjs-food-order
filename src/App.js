2c78dcddbbeed7fefc9172bb64420d897406f6ba
nargesssalehitajrishi-star <nargess.salehitajrishi@gmail.com>
2026-06-06 16:49:04 +0330

    added cart modal

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
 import { useState } from 'react';
-
-
 import Header from './components/Layout/Header';
 import Meals from './components/Meals/Meals';
 import CartProvider from './store/CartProvider';
+import Cart from './components/Cart/Cart';
 
 function App() {
   const [cartIsShown, setCartIsShown] = useState(false);
@@ -15,9 +14,10 @@ function App() {
     setCartIsShown(false);
   }
 
-
+  function App() { }
   return (
     <CartProvider>
+      <Cart />
       {cartIsShown && <div>Cart...</div>}
       <Header onShowCart={showCarttHandler} />
       <main>
@@ -26,5 +26,7 @@ function App() {
     </CartProvider>
   );
 }
+
+
 
 export default App;
